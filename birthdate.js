3ea125275e4c4dcfea0a67a2578fed59bd2198a2
Manoj Asarsa <manojasarsa7611@gmail.com>
2021-09-08 13:51:57 +0530

    docs: Added bonus part: Alert and Footer

--- birthdate.js
+++ birthdate.js
@@ -1,15 +1,17 @@
 const dob = document.querySelector("#dob");
 const luckyNumber = document.querySelector("#lucky-number");
 const checkBtn = document.querySelector("#check-btn");
+const privacyBtn = document.querySelector("#privacy-btn");
+const privacyNote = document.querySelector("#privacy-note");
 const output = document.querySelector("#output");
 
 
 
 function findSumOfDate(birthDate) {
 
-    const validDate = birthDate.replaceAll("-","")
+    const validDate = birthDate.replaceAll("-", "")
     var sum = 0;
-    for ( let num of validDate) {
+    for (let num of validDate) {
         sum = sum + Number(num)
     }
     return sum;
@@ -17,14 +19,23 @@ function findSumOfDate(birthDate) {
 
 
 function findBithdateIsLucky() {
-    const sum = findSumOfDate(dob.value);
-    
-    if ( sum % luckyNumber.value == 0) {
-        output.innerText = "YAY! Your birthday is lucky!! 😍";
+    if (dob.value && luckyNumber.value>0) {
+        const sum = findSumOfDate(dob.value);
+
+        if (sum % luckyNumber.value == 0) {
+            output.innerText = "YAY! Your birthday is lucky!! 😍";
+        } else {
+            output.innerText = "Oops!! Your birthday is not lucky!! 😔";
+        }
     }
-    else {
-        output.innerText = "Oops!! Your birthday is not lucky!! 😔";
+    else{
+        output.innerText = "Please enter valid inputs!";
     }
+
 }
 
 checkBtn.addEventListener("click", findBithdateIsLucky);
+
+privacyBtn.addEventListener("click", () => {
+    privacyNote.style.display = "none";
+})
